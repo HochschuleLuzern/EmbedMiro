13a0dd0b7c5f6cb25bb277988fdb3c6a8b5c0b19
Stephan Winiker <stephan.winiker@hslu.ch>
2022-03-23 16:50:05 +0100

    Fix Plugin Info

--- plugin.php
+++ plugin.php
@@ -1,9 +1,9 @@
 <?php
-	$id='xmiro';
-	$version='0.0.1';
+	$id = 'xmiro';
+	$version = '0.0.1';
 
-	$ilias_min_version='7.0';
-	$ilias_max_version='7.999';
+	$ilias_min_version = '7.0';
+	$ilias_max_version = '7.999';
 
-	$responsible='Stephan Winiker';
-	$responisble_mail='stephan.winiker@hslu.ch';
+	$responsible = 'Stephan Winiker';
+	$responsible_mail = 'stephan.winiker@hslu.ch';
