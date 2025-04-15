01b3f95a125b52c5e01f4406f8148d25aaa93fc8
msalterhslu <mark.salter@hslu.ch>
2025-04-15 15:02:12 +0200

    Compatibility with ILIAS 9

--- plugin.php
+++ plugin.php
@@ -1,9 +1,9 @@
 <?php
 	$id = 'xmiro';
-	$version = '0.0.2';
+	$version = '0.0.3';
 
-	$ilias_min_version = '7.0';
-	$ilias_max_version = '8.999';
+    $ilias_min_version = "8.0";
+    $ilias_max_version = "9.999";
 
 	$responsible = 'Mark Salter';
 	$responsible_mail = 'mark.salter@hslu.ch';
